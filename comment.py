2f232a300a6f0ac5c0cac314403c704cbf37c469
Anand-Bheesetti <anand.bheesetti@techolution.com>
2025-09-24 17:48:46 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -1,6 +1,38 @@
 import json
 from datetime import datetime
 
+import os
+import sys
+
+
+API_KEY = os.getenv("NEW_API_KEY")   
+DB_URL = os.getenv("NEW_DATABASE_URL") 
+
+def fun1(a,b):   
+    c=a+b  
+    if c>10:print("greater") 
+    return c
+
+def check_api():
+    if API_KEY == None: 
+        print("No API Key provided!")  
+    else:
+        print("API Key length is", len(API_KEY))
+
+class myclass: 
+    def __init__(self,x):
+        self.x=x  
+    def prnt(self):print(self.x)  
+
+
+for i in range(5):
+    val==i*2   
+
+check_api()
+obj=myclass(42)
+obj.prnt()
+
+
 def save_user_data(user_id: int, name: str, email: str, filename: str = "users.json"):
     """Save user data to a JSON file."""
     user_record = {
