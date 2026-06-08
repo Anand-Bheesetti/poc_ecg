12b01f312acd2e5c210437b2f953cf9e0f679d62
Anand-Bheesetti <anand.bheesetti@techolution.com>
2026-06-08 23:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 
 st.title("POC For ECG")
 problem=st.text_input("Enter the problem statement")
