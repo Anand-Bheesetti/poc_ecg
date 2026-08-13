be7ceab6cb9e3dd922f12806f320e7615e72dc0e
Anand-Bheesetti <anand.bheesetti@techolution.com>
2026-08-13 14:46:53 +0530

    Update a.py

--- a.py
+++ a.py
@@ -3,6 +3,6 @@ if (number%2==0):
   print("even")
 else:
   print("odd")
-print("hi hellr")
+print("hi helol")
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost:5432/appdb")
