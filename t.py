fe12adc7f15787d0940fe6c5d1c5c72d1bb06851
Anand-Bheesetti <anand.bheesetti@techolution.com>
2026-08-17 15:34:16 +0530

    Update t.py

--- t.py
+++ t.py
@@ -6,7 +6,7 @@ API_KEY = os.getenv("NEW_API_KEY")
 DB_URL = os.getenv("NEW_DATABASE_URL")  
 
 print("HI hellolo")
-print("HI wyu hi hello how are you......")
+print("HI wyu hi hello how are you...... this is fine")
 
 
 def fun1(a,b):   
